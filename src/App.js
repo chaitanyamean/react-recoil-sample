b1353761c9b36884cbdfbba66f18fa2f7d3d0834
chaitanyamean <chaitanyamean@gmail.com>
2020-07-12 17:10:23 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {HashRouter as Router, Route, Switch} from 'react-router-dom';
+import Header from './components/Header';
+import Cart from './components/Cart';
+import Catlog from './components/Catlog';
+import {RecoilRoot} from 'recoil'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <RecoilRoot>
+
+      <Router basename='/'>
+      <Header />
+      <Switch>
+        <Route  exact path='/' component={Catlog}/>
+        <Route  path='/cart' component={Cart}/>
+      </Switch>
+      </Router>
+      </RecoilRoot>
     </div>
   );
 }
---
+++ src/store/index.js
@@ -0,0 +1,5 @@
+export {cart} from './atoms';
+
+export {cartState} from './selectors';
+
+export {useAddItem, useDecreaseItem, useRemoveItem} from './hooks';
